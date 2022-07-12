af6a0b3658719ff5fe14b11f5a1b00a5bc94a52f
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-07-12 04:58:36 +0000

    Add default status listener

---
+++ src/Listeners/CreateDefaultStatuses.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Spork\Planning\Listeners;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Str;
+
+class CreateDefaultStatuses 
+{
+    protected const STATUSES = [
+        'Backlog',
+        'To do',
+        'In Progress',
+        'Done',
+    ];
+
+    public function handle(Registered $event)
+    {
+        $this->createStatuses($event->user);
+    }
+
+    protected function createStatuses(User $user)
+    {
+        foreach (static::STATUSES as $order => $status) {
+            $user->statuses()->create([
+                'title' => $status,
+                'slug' => Str::of($status)->slug(),
+                'order' => $order,
+            ]);
+        }
+    }
+}
