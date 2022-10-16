604a7cc19604a053db65dd54368ad40d545b13ed
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-10-15 23:31:31 -0400

    Fix PHPUnit.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,7 @@ namespace Spork\Planning\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as TestbenchTestCase;
-use Spork\Planning\PlanningServiceProvider.php;
+use Spork\Planning\PlanningServiceProvider;
 
 class TestCase extends TestbenchTestCase
 {
@@ -20,7 +20,7 @@ class TestCase extends TestbenchTestCase
     protected function getPackageProviders($app)
     {
         return [
-            PlanningServiceProvider.php::class,
+            PlanningServiceProvider::class,
         ];
     }
 }
