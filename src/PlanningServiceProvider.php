f372defdcf8f5305db6520b0ed5dad46c8037a19
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-11-24 23:51:27 -0500

    Update workflow and the config

--- src/PlanningServiceProvider.php
+++ src/PlanningServiceProvider.php
@@ -15,6 +15,7 @@ class PlanningServiceProvider extends RouteServiceProvider
     public function register()
     {
         Spork::addFeature('planning', 'ViewBoardsIcon', '/planning/kanban', 'tool');
+        $this->mergeConfigFrom(__DIR__ . '/../config/spork.php', 'spork.planning');
         if (config('spork.planning.enabled')) {
             Route::middleware($this->app->make('config')->get('spork.planning.middleware', ['auth:sanctum']))
                 ->prefix('api/planning')
